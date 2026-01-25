db4903fb1a6dcc15a01465acd8f65a9a99b9aa4e
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-24 21:45:50 -0500

    Section 13 - Class 144: Assignment Review - Create Recipe Project

---
+++ src/main/java/com/stmc/sfgrecipeapp5/SfgRecipeApp5Application.java
@@ -0,0 +1,13 @@
+package com.stmc.sfgrecipeapp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeApp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecipeApp5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/stmc/sfgrecipeapp5/SfgRecipeApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stmc.sfgrecipeapp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeApp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
