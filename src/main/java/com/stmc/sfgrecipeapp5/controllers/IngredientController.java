224e8083c25155e8bd74da7d209ce0b07c1124a3
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-28 21:55:58 -0500

    Section 17 - Class 246: View Ingredients

---
+++ src/main/java/com/stmc/sfgrecipeapp5/controllers/IngredientController.java
@@ -0,0 +1,28 @@
+package com.stmc.sfgrecipeapp5.controllers;
+
+import com.stmc.sfgrecipeapp5.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable("recipeId") String recipeId, Model model) {
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findRecipeCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
---
+++ src/test/java/com/stmc/sfgrecipeapp5/controllers/IngredientControllerTest.java
@@ -0,0 +1,47 @@
+package com.stmc.sfgrecipeapp5.controllers;
+
+import com.stmc.sfgrecipeapp5.commands.RecipeCommand;
+import com.stmc.sfgrecipeapp5.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listIngredients() throws Exception {
+        // Given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findRecipeCommandById(anyLong())).thenReturn(recipeCommand);
+
+        // When
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        // Then
+        verify(recipeService, times(1)).findRecipeCommandById(anyLong());
+    }
+}
