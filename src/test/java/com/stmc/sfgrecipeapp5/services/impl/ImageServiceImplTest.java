85c4cd72c29ab13c71e2ba83d5fde1fbb81dc797
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-29 01:47:55 -0500

    Section 17 - Class 255: Persisting images to Database

---
+++ src/test/java/com/stmc/sfgrecipeapp5/services/impl/ImageServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.stmc.sfgrecipeapp5.services.impl;
+
+import com.stmc.sfgrecipeapp5.model.Recipe;
+import com.stmc.sfgrecipeapp5.repositories.RecipeRepository;
+import com.stmc.sfgrecipeapp5.services.ImageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+class ImageServiceImplTest {
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void saveImageFile() throws IOException {
+        // Given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain", "Spring Framework Guru".getBytes());
+
+        Recipe  recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        // When
+        imageService.saveImageFile(id, multipartFile);
+
+        // Then
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+}
