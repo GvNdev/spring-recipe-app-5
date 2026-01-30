ea1553021592bbe6b4b9f3bd51ed85794665f181
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-30 09:42:24 -0500

    Section 18 - Class 272: Spring MVC Exception Handler

--- src/main/java/com/stmc/sfgrecipeapp5/controllers/RecipeController.java
+++ src/main/java/com/stmc/sfgrecipeapp5/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.stmc.sfgrecipeapp5.controllers;
 
 import com.stmc.sfgrecipeapp5.commands.RecipeCommand;
+import com.stmc.sfgrecipeapp5.exceptions.NotFoundException;
 import com.stmc.sfgrecipeapp5.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -46,5 +49,15 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 }
